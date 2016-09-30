65121b0a442e84140dee4276f1d3f319e84a02d2
tomfun <tomfun1990@gmail.com>
2016-09-30 22:35:26 +0300

    validation

--- src/Entity/ValueLocation.php
+++ src/Entity/ValueLocation.php
@@ -4,6 +4,7 @@ namespace Brander\Bundle\EAVBundle\Entity;
 use Brander\Bundle\EAVBundle\Model\GeoLocation;
 use Doctrine\ORM\Mapping as ORM;
 use JMS\Serializer\Annotation as Serializer;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * Значение аттрибута для положения на карте
@@ -17,6 +18,13 @@ class ValueLocation extends Value
 {
     const ELASTICA_POSTFIX = '_eavpnt_geo';
     /**
+     * @Serializer\Accessor(getter="getValue",setter="setValue")
+     * @Serializer\Type("Brander\Bundle\EAVBundle\Model\GeoLocation")
+     * @Assert\Regex(pattern="/^-?\d+([.]\d+)?,-?\d+([.]\d+)?$/", message="must be a valid number")
+     */
+    protected $value;
+    /**
+     * @Assert\Valid()
      * @var GeoLocation
      */
     protected $valueTyped = null;
@@ -44,6 +52,7 @@ class ValueLocation extends Value
             }
         }
 
+//        return '#######';
         return $this->valueTyped;
     }
 }
--- src/Entity/ValueNumeric.php
+++ src/Entity/ValueNumeric.php
@@ -3,6 +3,7 @@ namespace Brander\Bundle\EAVBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
 use JMS\Serializer\Annotation as Serializer;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * Значение аттрибута для numeric
@@ -14,5 +15,8 @@ use JMS\Serializer\Annotation as Serializer;
  */
 class ValueNumeric extends Value
 {
-
+    /**
+     * @Assert\Regex(pattern="/^-?\d+(\.\d+)?$/", message="must be a valid number")
+     */
+    protected $value;
 }
