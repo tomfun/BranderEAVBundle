bcd040c77f9bcfaf388c3154f048ba912b98eb06
Остапчук Алина <alinyonish@gmail.com>
2016-12-23 15:44:41 +0200

    populate action

---
+++ src/Controller/PopulateController.php
@@ -0,0 +1,40 @@
+<?php
+namespace Brander\Bundle\EAVBundle\Controller;
+
+use FOS\RestBundle\Controller\Annotations as Rest;
+use JMS\DiExtraBundle\Annotation as DI;
+use Symfony\Component\HttpFoundation\Response;
+use Nelmio\ApiDocBundle\Annotation\ApiDoc;
+use Brander\Bundle\EAVBundle\Service\PopulateService;
+
+/**
+ * @author mom <alinyonish@gmail.com>
+ * @Rest\Route("/eav")
+ */
+class PopulateController
+{
+    /**
+     * @DI\Inject("brander_eav.reindex_elastica")
+     * @var PopulateService
+     */
+    private $reindexEvent;
+
+    /**
+     * Elastica populate indexes
+     * @ApiDoc(
+     *     statusCodes={
+     *         204="Returned when successful"
+     *     }
+     * )
+     * @Rest\Patch("/reindex", name="shatel_product_reindex",
+     *      defaults={"_format": "json"}
+     * )
+     * @return Response
+     */
+    public function reindexAction()
+    {
+        $this->reindexEvent->reindex();
+
+        return (new Response())->setStatusCode(Response::HTTP_NO_CONTENT);
+    }
+}
